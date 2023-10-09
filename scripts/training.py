ea447af11e5720af2f346ed9dbb0e0c9bfa05552
Janis Fluri <janis.fluri@hotmail.com>
2023-10-09 05:05:18 +0200

    added different architectures

--- scripts/training.py
+++ scripts/training.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from torch import optim, nn
 from tpu_graph.data import TileDataset, LayoutDataset
-from tpu_graph.networks import TPUGraphNetwork, RetentiveAttention, SAGEConv
+from tpu_graph.networks import TPUGraphNetwork, SAGEConv  # RetentiveAttention
 from tpu_graph.training import evaluation
 from tpu_graph.training.ltr.pairwise_losses import PairwiseDCGHingeLoss
 from tqdm import tqdm
@@ -130,10 +130,15 @@ def train_tile_network(**kwargs):
     # the position embedding
     input_dim += 16
 
+    # message_network = nn.Sequential(
+    #     SAGEConv(256, 128), SAGEConv(128, 128), SAGEConv(128, 128), RetentiveAttention(128, 128)
+    # )
     message_network = nn.Sequential(
-        SAGEConv(256, 128), SAGEConv(128, 128), SAGEConv(128, 64), RetentiveAttention(64, 64)
+        SAGEConv(256, 128),
+        SAGEConv(128, 128),
+        SAGEConv(128, 128),
     )
-    projection_network = nn.Linear(64, 1)
+    projection_network = nn.Linear(128, 1)
 
     network = TPUGraphNetwork(
         in_channels=input_dim,
