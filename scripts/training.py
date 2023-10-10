99cfacb23b55f24c143c850708d1c7221c7b8538
Janis Fluri <janis.fluri@hotmail.com>
2023-10-11 00:05:48 +0200

    sync

--- scripts/training.py
+++ scripts/training.py
@@ -173,10 +173,10 @@ def train_network(rank, kwargs):
                 scheduler.step()
 
             # log the loss to the logger
-            total_loss = loss.detach()
+            total_loss = loss.detach() / kwargs["world_size"]
             if rank == 0:
                 # get the total loss
-                total_loss = dist.reduce(total_loss, dst=0, op=dist.ReduceOp.SUM, async_op=False) / kwargs["world_size"]
+                dist.reduce(total_loss, dst=0, op=dist.ReduceOp.SUM, async_op=False)
 
                 # set postfix
                 pbar.set_postfix({"loss": total_loss.item()})
