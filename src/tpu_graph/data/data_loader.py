b8907c23251bcabef435f2af887880c1ca7a55c4
Janis Fluri <janis.fluri@hotmail.com>
2023-11-15 14:11:24 +0100

    jet another message

--- src/tpu_graph/data/data_loader.py
+++ src/tpu_graph/data/data_loader.py
@@ -842,6 +842,9 @@ class LayoutDatasetV4(LayoutDataset):
         config_feat = data["node_config_feat"][indices] / 5.0
         node_config_ids = data["node_config_ids"]
         print("Read config features")
+        print(config_feat.shape)
+        print(node_config_ids.shape)
+        print(config_feat.shape)
 
         # fill the config features
         config_feat = fill_config_feat(len(node_feat), node_config_ids, config_feat)
